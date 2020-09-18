1aec47ea882db064b273df631b2d0210423d718f
Mourtada Imam <mortada255@gmail.com>
2020-09-18 07:38:23 +0100

    final

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views 
+urlpatterns = [
+    path('', views.home , name='blog-home'),
+    path('about/',views.about, name='blog-about'),
+    path('ope/',views.ope, name='ope'),
+    path('ope2/',views.ope2, name='ope2'),
+    path('ope3/',views.ope3, name='ope3'),
+    path('ope4/',views.ope4, name='ope4'),
+    path('ope5/',views.ope5, name='ope5'),
+    path('ope6/',views.ope6, name='ope6'),
+    path('ope7/',views.ope7, name='ope7'),
+    path('ope8/',views.ope8, name='ope8'),
+    path('ope9/',views.ope9, name='ope9'),
+    path('ope10/',views.ope10, name='ope10'),
+]
---
+++ blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse  
+# Create your views here.
+
+def home (request): 
+	
+	return render(request, 'blog/home.html')
+def about (request):
+	return render(request,'blog/about.html')
+def ope (request):
+	return render(request,'blog/ope.html')
+def ope2(request):
+	return render(request,'blog/ope2.html')
+def ope3(request):
+	return render(request,'blog/ope3.html')
+def ope4(request):
+	return render(request,'blog/ope4.html')
+def ope5(request):
+	return render(request,'blog/ope5.html')
+def ope6(request):
+	return render(request,'blog/ope6.html')
+def ope7(request):
+	return render(request,'blog/ope7.html')
+
+def ope8(request):
+	return render(request,'blog/ope8.html')
+def ope9(request):
+	return render(request,'blog/ope9.html')
+def ope10(request):
+	return render(request,'blog/ope10.html')	
